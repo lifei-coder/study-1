a49ba37440d35c1845f50e9e045a34bf7c29d8f4
lifei <lifei@hualala.com>
2018-11-26 18:59:00 +0800

    内部类

---
+++ src/main/java/exercise/InnerClass/AnonymousInnerClassTest.java
@@ -0,0 +1,32 @@
+package exercise.InnerClass;
+
+public class AnonymousInnerClassTest {
+
+    public static void main(String[] args) {
+        // 匿名内部类方式 创建对象
+        // 匿名内部类就是重写父类或接口的方法
+        Bird bird = new Bird() {
+            @Override
+            public int fly() {
+                return 1000;
+            }
+
+            @Override
+            public String getName() {
+                return "大雁";
+            }
+        };
+        System.out.println(bird.getName() + "能够飞" + bird.fly() + "米");
+    }
+
+
+}
+// 这里可以是普通类或者抽象类
+abstract class Bird{
+    private String name;
+    // 定义一个抽象方法
+    public abstract int fly();
+
+    public String getName(){return name;};
+    public void setName(String name){this.name = name;}
+}
---
+++ src/main/java/exercise/InnerClass/InnerClassTest.java
@@ -0,0 +1,41 @@
+package exercise.InnerClass;
+
+public class InnerClassTest {
+
+    public static void main(String[] args) {
+        PersonPrinter personPrinter = new PersonPrinter(18, '男', "大学本科");
+        personPrinter.printPerson();
+    }
+
+}
+
+// 这不是内部类，只是为了少写一个main方法的java文件
+class PersonPrinter {
+
+    private int age;
+    private char sex;
+    private String degree;
+
+    public PersonPrinter(int age, char sex, String degree) {
+        this.age = age;
+        this.sex = sex;
+        this.degree = degree;
+    }
+
+    public void printPerson(){
+        Worker worker = new Worker();
+        worker.workerInfo();
+    }
+
+    // an inner class
+    public class Worker{
+        public void workerInfo(){
+            if (age >= 18) {
+                System.out.println("我是一名工人, age:" + age);
+            } else {
+                System.out.println("我是一名小孩子, age:" + age);
+            }
+        }
+    }
+
+}
