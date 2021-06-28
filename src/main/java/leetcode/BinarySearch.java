3ebf62d2221818082a4d37973fe6370f06e06975
lifei <lifei1@songguo.com>
2021-06-28 20:52:15 +0800

    重写二分查找

--- src/main/java/leetcode/BinarySearch.java
+++ src/main/java/leetcode/BinarySearch.java
@@ -1,32 +1,28 @@
 package leetcode;
 
 public class BinarySearch {
+
     public static void main(String[] args) {
-        int[] list = {2,5,7,8,9,22,123,555,777,798};
-        System.out.println("找到元素index 为: " + bsearch(list, 7));
+        int[] list = {2, 5, 7, 8, 9, 22, 123, 555, 777, 798};
+        System.out.println("找到元素index 为: " + bsearch(list, 22));
     }
 
-
-    private static int bsearch(int[] a, int value) {
-
-        int low = 0;
-        int high = a.length-1;
-        while (low <= high) { // 这里 low <= high
-            int mid = (low + high) / 2;
-            if (a[mid] == value) {
+    private static int  bsearch(int[] list, int target) {
+        int left = 0;
+        int right = list.length - 1;
+        int mid = 0;
+        while (left <= right) {
+             mid = (left+right)/2;
+            if (list[mid] == target) {
                 return mid;
-            } else if (a[mid] < value) {
-                low = mid + 1;
+            } else if (list[mid] > target) {
+                right = mid - 1;
             } else {
-                high = mid - 1;
+                left = mid + 1;
             }
         }
-
-
         return -1;
     }
-
-
 
 
 }
