8695bac5729e79979c5efb159b0c8564bceaae38
lifei <lifei1@songguo.com>
2021-06-28 20:25:33 +0800

    再写一遍快排

--- src/main/java/leetcode/QuickSort.java
+++ src/main/java/leetcode/QuickSort.java
@@ -6,23 +6,23 @@ public class QuickSort {
 
     // 快排地推公式： quick_sort = quick_sort(p, ..., q-1) + quick_sort(q+1, ..., r)
     public static void main(String[] args) {
-        int[] list = {5, 7, 3, 8, 13, 9, 1};
+        int[] list = {5, 7, 3, 8, 13, 4};
         quickSort(list, 0, list.length-1);
         System.out.println(Arrays.toString(list));
     }
 
-    public static void quickSort(int[] list, int left, int right) {
+    private static void quickSort(int[] list, int left, int right) {
         if (left >= right)
             return;
         int midIndex = partition(list, left, right);
-        quickSort(list, 0, midIndex-1);
+        quickSort(list, left, midIndex - 1);
         quickSort(list, midIndex + 1, right);
     }
 
-    private static int partition(int[] list, int p, int r) {
-        int pivot = list[r];
-        int i = p;
-        for (int j = p; j < list.length; j++) {
+    private static int partition(int[] list, int left, int right) {
+        int pivot = list[right];
+        int i = left;
+        for (int j = left; j < list.length; j++) {
             if (list[j] < pivot) {
                 int temp = list[j];
                 list[j] = list[i];
@@ -30,8 +30,8 @@ public class QuickSort {
             }
         }
         int temp = list[i];
-        list[i] = list[r];
-        list[r] = temp;
+        list[i] = list[right];
+        list[right] = temp;
         return i;
     }
 
