4a5f13741ea33887bc10834238b6292e05fe2a8f
lifei <lifei1@songguo.com>
2021-06-28 15:50:33 +0800

    再写一遍快排

--- src/main/java/leetcode/QuickSort.java
+++ src/main/java/leetcode/QuickSort.java
@@ -6,39 +6,34 @@ public class QuickSort {
 
     // 快排地推公式： quick_sort = quick_sort(p, ..., q-1) + quick_sort(q+1, ..., r)
     public static void main(String[] args) {
-        int[] list = {5,7,3,8,13,9};
+        int[] list = {5, 7, 3, 8, 13, 9, 1};
         quickSort(list, 0, list.length-1);
         System.out.println(Arrays.toString(list));
     }
 
-    public static void quickSort(int[] array, int start, int end) {
-        if (start >= end) {
+    public static void quickSort(int[] list, int left, int right) {
+        if (left >= right)
             return;
-        }
-        int mid = partition(array, start, end);
-        quickSort(array, start, mid-1);
-        quickSort(array, mid+1, end);
+        int midIndex = partition(list, left, right);
+        quickSort(list, 0, midIndex-1);
+        quickSort(list, midIndex + 1, right);
     }
 
-    public static int partition(int[] array, int start, int end) {
-        int pivot = array[end];
-        int i = start;
-        for (int j = start; j < array.length; j++) {
-            if (array[j] < pivot) {
-                if (i == j) {
-                    i++;
-                } else {
-                    int tmp = array[i];
-                    array[i++] = array[j];
-                    array[j] = tmp;
-                }
+    private static int partition(int[] list, int p, int r) {
+        int pivot = list[r];
+        int i = p;
+        for (int j = p; j < list.length; j++) {
+            if (list[j] < pivot) {
+                int temp = list[j];
+                list[j] = list[i];
+                list[i++] = temp;
             }
         }
-        // 交换Array[i] 跟 Array[end]
-        int tmp = array[i];
-        array[i] = array[end];
-        array[end] = tmp;
+        int temp = list[i];
+        list[i] = list[r];
+        list[r] = temp;
         return i;
     }
+
 
 }
